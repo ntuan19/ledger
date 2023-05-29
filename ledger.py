f91de1d911e68ee4236f9a444d005a7cc1e73a63
ntuan19 <nanhtuan.fly@gmail.com>
2023-05-28 21:49:21 -0700

    Creating changes/ Without applying interests

--- ledger.py
+++ ledger.py
@@ -1,44 +1,58 @@
-class Ledger(object):
-    def __init__(self,transactions):
-        self.transactions = transactions
-        self.balances = {}
-        self.receivable_loans= 0
-
-    def execute_transactions(self):
-        self.balances = {}
-        for transaction in self.transactions:
-            amount = transaction.amount
-            if transaction.from_account in self.balances:
-                if transaction.transaction_type == "CREATE_LOAN":
-                    self.balances[transaction.from_account] += amount
-                elif transaction.transaction_type == "LOAN_PAYMENT":
-                    self.balances[transaction.from_account] -= amount
-            else:
-                self.balances[transaction.from_account] = 0
-    
-    def get_loan_receivable(self,loan_name):
-        balances = self.balances
-        if loan_name in balances:
-            return balances[loan_name]
-        else:
-            return "None"
-    
-    def get_loans_receivable_balance(self):
-        for transaction in self.transactions:
-            amount = transaction.amount
-            if transaction.transaction_type == "CREATE_LOAN":
-                self.receivable_loans+=amount
-        return self.receivable_loans
-           
-
-        
-
-
-
-class Transaction(object):
-    def __init__(self,from_account, to_account,amount,transaction_type):
+from collections import defaultdict
+class Transaction():
+    def __init__(self,from_account,to_account,amount,transaction_type) -> None:
         self.from_account = from_account
         self.to_account = to_account
         self.amount = amount
         self.transaction_type = transaction_type
+
+
+class Ledger(object):
+    def __init__(self,transactions):
+        self.accounts_names = defaultdict(int)
+        self.transactions = transactions
+
+    def execute_transactions(self):
+        for transaction in self.transactions:
+
+            payer = transaction.from_account
+            receiver = transaction.to_account
+            total_amount  = transaction.amount
+            type_transaction = transaction.transaction_type
+
+            # if from_account not in self.accounts_names:
+            #     self.accounts_names[from_account]
+            # if to_account not in self.accounts_names:
+            #     self.accounts_names[to_account]
+            
+            if type_transaction == "LOAN_PAYMENT":
+                self.accounts_names[payer] -= total_amount
+                self.accounts_names[receiver] += total_amount
+            elif type_transaction == "CREATE_LOAN":
+                self.accounts_names[payer] -= total_amount
+                self.accounts_names[receiver] += total_amount
+    
+    def get_loan_receivable(self,account):
+        if account not in self.accounts_names:
+             return "Not found"
+        return self.accounts_names[account]
+    
+    def get_loans_receivable_balance(self):
+        total_loan = 0
+        for value in self.accounts_names.values():
+            if value <0:
+                total_loan -=value
+        return total_loan
+
+
+    
+            
+            
+
+  
+    
+
+
+
+
         
